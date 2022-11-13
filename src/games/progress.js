74ea107fd296f07a364e9d6aae692e49cdeea3d1
Marina Kucheryavenko <kmskms84@mail.ru>
2022-11-14 00:33:17 +0300

    fix progress.js

--- src/games/progress.js
+++ src/games/progress.js
@@ -3,28 +3,22 @@ import runBrainGames from '../index.js';
 
 const rule = 'What number is missing in the progression?';
 
-const progression = (startNum, step, position) => {
-  let corrAnswer = '';
-  let arithProgression = '';
-  let start = startNum;
+const createProgression = (startNum, step) => {
+  const arithProgression = [];
   for (let i = 0; i < 10; i += 1) {
-    if (i !== position) {
-      arithProgression = `${arithProgression}${start} `;
-      start += step;
-    } else {
-      arithProgression = `${arithProgression}${'..'} `;
-      corrAnswer = start;
-      start += step;
-    }
+    arithProgression.push(startNum + (step * i));
   }
-  return [arithProgression, corrAnswer];
+  return arithProgression;
 };
 
 const getQuestionAnswer = () => {
   const firstNumber = getRandomIntInclusive(0, 100);
   const stepProgression = getRandomIntInclusive(1, 10);
   const emptyPosition = getRandomIntInclusive(0, 9);
-  const [question, corrAnswer] = progression(firstNumber, stepProgression, emptyPosition);
+  const progression = createProgression(firstNumber, stepProgression);
+  const corrAnswer = progression[emptyPosition];
+  progression[emptyPosition] = '..';
+  const question = progression.join(' ');
   return [question, corrAnswer];
 };
 
