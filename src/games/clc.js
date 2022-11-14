660d3149ab560d2fbca05ec3ecd71c999489bd03
Marina Kucheryavenko <kmskms84@mail.ru>
2022-11-14 16:49:27 +0300

    rename calculation() -> calculate()

--- src/games/clc.js
+++ src/games/clc.js
@@ -3,7 +3,7 @@ import runBrainGames from '../index.js';
 
 const rule = 'What is the result of the expression?';
 const mathOperations = ['+', '-', '*'];
-const calculation = (x, y, symbol) => {
+const calculate = (x, y, symbol) => {
   switch (symbol) {
     case '+':
       return x + y;
@@ -21,7 +21,7 @@ const getQuestionAnswer = () => {
   const secondNumber = getRandomIntInclusive(0, 10);
   const numOperation = getRandomIntInclusive(0, mathOperations.length - 1);
   const mathSymbol = mathOperations[numOperation];
-  const corrAnswer = calculation(firstNumber, secondNumber, mathSymbol);
+  const corrAnswer = calculate(firstNumber, secondNumber, mathSymbol);
   const question = `${firstNumber} ${mathSymbol} ${secondNumber}`;
   return [question, corrAnswer.toString()];
 };
